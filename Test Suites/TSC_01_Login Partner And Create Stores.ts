ece6700bf57cda518b4b9fb4711d3bdb7d5ee610
lhv137 <lehoangvidt@gmail.com>
2023-07-28 16:43:40 +0700

    update test suites

--- Test Suites/TSC_01_Login Partner And Create Stores.ts
+++ Test Suites/TSC_01_Login Partner And Create Stores.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>TSC_01_Login Partner</name>
+   <name>TSC_01_Login Partner And Create Stores</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
---
+++ Test Suites/TS_Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f45b749d-aaf2-453f-821c-4ac0ced30cb9</testSuiteGuid>
+</TestSuiteEntity>
